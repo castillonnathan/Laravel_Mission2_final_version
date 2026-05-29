5fed54366e5f4733b0e613ed9d5b1fd2b619ec6e
castillonnathan <castillonnathan4@gmail.com>
2026-05-29 10:56:21 +0200

    final version

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -449,6 +449,7 @@
             const el = document.getElementById('tab-' + tabId);
             el.style.display = 'flex';
             el.style.flexDirection = 'column';
+            const z = a;
             btn.classList.add('active');
         }
 
